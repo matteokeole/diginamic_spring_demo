56f7cd17a989f0e9af30ec12872117fc42fdfb67
Mattéo (Diginamic) <matteo@studiokg.fr>
2023-11-09 16:06:24 +0100

    feat: Species controller

--- src/main/java/com/diginamic/demo/controller/SpeciesController.java
+++ src/main/java/com/diginamic/demo/controller/SpeciesController.java
@@ -1,25 +1,57 @@
 package com.diginamic.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.diginamic.demo.entity.Species;
 import com.diginamic.demo.repository.SpeciesRepository;
 
 @Controller
+@RequestMapping("/species")
 public class SpeciesController {
 	@Autowired
 	private SpeciesRepository speciesRepository;
 
-	@GetMapping("/species")
-	public String listSpecies(final Model model) {
+	@GetMapping("")
+	public String list(final Model model) {
 		final List<Species> species = speciesRepository.findAll();
 
 		model.addAttribute("species", species);
 
-		return "list_species";
+		return "species/list";
+	}
+
+	@RequestMapping(path={"/add"}, method={RequestMethod.GET, RequestMethod.POST})
+	public String add(final Model model, final HttpServletRequest request) {
+		final Species species = new Species();
+
+		if (request.getMethod().equals("POST")) {
+			species.setCommonName(request.getParameter("commonName"));
+			species.setLatinName(request.getParameter("latinName"));
+
+			speciesRepository.save(species);
+
+			return "redirect:/species";
+		}
+
+		model.addAttribute("species", species);
+
+		return "species/add";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") Long speciesId, final Model model) {
+		final Species species = speciesRepository.findById(speciesId).get();
+
+		model.addAttribute("species", species);
+
+		return "species/show";
 	}
 }
