e2c9b9ab463a739a0af333f8e63eb4cc7f7b0de1
Mattéo (Diginamic) <matteo@studiokg.fr>
2023-11-10 17:20:42 +0100

    feat: Person controller

---
+++ src/main/java/com/diginamic/demo/controller/PersonController.java
@@ -0,0 +1,54 @@
+package com.diginamic.demo.controller;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.diginamic.demo.entity.Person;
+import com.diginamic.demo.service.PersonService;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+	@Autowired
+	private PersonService personService;
+
+	@GetMapping
+	public Page<Person> findAll(final Pageable pageable) {
+		return personService.findAll(pageable);
+	}
+
+	@GetMapping("/{id}")
+	public Person find(@PathVariable("id") final Integer id) {
+		return personService
+			.find(id)
+			.orElseThrow(EntityNotFoundException::new);
+	}
+
+	@PostMapping
+	public void create(@RequestBody @Valid final Person person) {
+		personService.create(person);
+	}
+
+	@PutMapping
+	public void update(@RequestBody @Valid final Person person) {
+		personService.update(person);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") final Integer id) {
+		final Person person = personService.find(id).orElseThrow(EntityNotFoundException::new);
+
+		personService.delete(person);
+	}
+}
