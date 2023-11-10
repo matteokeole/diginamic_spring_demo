5658de33e597fbafbd52ed466213782aa67d0788
Mattéo (Diginamic) <matteo@studiokg.fr>
2023-11-10 16:55:17 +0100

    feat: SpeciesService

--- src/main/java/com/diginamic/demo/Application.java
+++ src/main/java/com/diginamic/demo/Application.java
@@ -1,9 +1,11 @@
 package com.diginamic.demo;
 
+import jakarta.transaction.Transactional;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
+@Transactional
 public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/diginamic/demo/service/SpeciesService.java
@@ -0,0 +1,43 @@
+package com.diginamic.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diginamic.demo.entity.Species;
+import com.diginamic.demo.repository.SpeciesRepository;
+
+@Service
+public class SpeciesService {
+	@Autowired
+	private SpeciesRepository speciesRepository;
+
+	public List<Species> findAll() {
+		return speciesRepository.findAll();
+	}
+
+	public List<Species> findByLatinNameIgnoreCaseContains(final String latinName) {
+		return speciesRepository.findByLatinNameIgnoreCaseContains(latinName);
+	}
+
+	public Optional<Species> find(final Integer id) {
+		return speciesRepository.findById(id);
+	}
+
+	public Optional<Species> findFirstByCommonName(final String commonName) {
+		return Optional.of(speciesRepository.findFirstByCommonName(commonName));
+	}
+
+	public void create(final Species species) {
+		speciesRepository.save(species);
+	}
+
+	public void update(final Species species) {
+		speciesRepository.save(species);
+	}
+
+	public void delete(final Species species) {
+		speciesRepository.delete(species);
+	}
+}
